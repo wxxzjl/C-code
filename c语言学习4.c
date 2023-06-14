85306853b0754ccd6b0c645ef028561ac8d9e0dd
张彧 <106905146+wxxzjl@users.noreply.github.com>
2023-06-14 22:38:07 +0800

    Update c语言学习4.c

--- c语言学习4.c
+++ c语言学习4.c
@@ -39,4 +39,35 @@ int main()
 }
 
 
-//
+//计算1/1-1/2+1/3-1/4+1/5...-1/100=
+#include <stdio.h>
+int main()
+{
+    int i=0;
+    double sum=0.0;
+    int flag=1;
+    for(i=1;i<=100;i++){
+        sum+= flag*1.0/i;      
+        flag=-flag;     //偶数位为-1
+    }
+    printf("%lf\n",sum);
+    return 0;
+}
+
+
+//打印九九乘法表
+#include <stdio.h>
+int main()
+{
+    int i=0;
+    int j=0;
+    int sum=1;
+    for(i=1;i<=9;i++){
+        for(j=1;j<=i;j++){
+            sum=i*j;
+            printf("%d*%d=%-2d ",j,i,sum);  //%-2d表示两位左对齐，%2d表示两位右对齐     
+        }
+         printf("\n");
+    }
+    return 0;
+}
