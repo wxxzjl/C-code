54e3712e547be37f5e4878c3cc3aea610351dc51
张彧 <106905146+wxxzjl@users.noreply.github.com>
2023-06-19 11:13:42 +0800

    Update c语言学习4.c

--- c语言学习4.c
+++ c语言学习4.c
@@ -128,4 +128,23 @@ int main()
    
 
 
-//
+//计算机关机程序
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(){
+    char input[20]={0};
+    //shutdown -s -t 60 系统60秒关机程序
+    //system() 执行系统命令的
+    system("shutdown -s -t 60");    //系统关机命令
+    again:
+    printf("请注意，你的电脑将在60秒后自动关机，如果输入：我是猪，就取消关机\n请输入：");
+    scanf("%s",input);
+    if(strcmp(input,"我是猪")==0)   //比较两个字符串strcmp
+    {
+        system("shutdown -a");     //系统取消关机命令
+    }else{
+        goto again;                //goto语句跳转
+    }
+    return 0;
+}
