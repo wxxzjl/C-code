5b0d9e48e053e2c91dec84dc7650cb5f32593400
张彧 <106905146+wxxzjl@users.noreply.github.com>
2023-07-04 17:28:22 +0800

    Update c语言学习4.c

--- c语言学习4.c
+++ c语言学习4.c
@@ -193,3 +193,34 @@ for(int i=0;i<9;i++){
 }
 return 0;
 }
+
+//指针数组和函数
+//封装一个函数，利用冒泡排序，实现对整形数组的升序排序
+#include <stdio.h>
+//冒泡排序函数
+void bubbleSort(int *arr,int len){
+    for(int i=0;i<len-1;i++){
+        for(int j=0;j<len-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+//打印数组
+void printArr(int *arr,int len){
+    for(int i=0;i<len;i++){
+        printf("%d\n",arr[i]);
+    }
+}
+int main(){
+    //创建数组
+    int arr[]={4,3,6,9,1,2,10,8,7,5};
+    //计算数组长度
+    int len=sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr,len);    //实参更改了
+    printArr(arr,len);      //遍历数组
+    return 0;
+}
