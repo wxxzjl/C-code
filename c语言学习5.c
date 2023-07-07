11ca5b148b056c23a00040ac339876182c307439
张彧 <106905146+wxxzjl@users.noreply.github.com>
2023-07-07 16:55:35 +0800

    Update c语言学习5.c

--- c语言学习5.c
+++ c语言学习5.c
@@ -58,6 +58,7 @@ int main() {
 
 
 //计算第n个斐波那契数
+//1 1 2 3 5 8 13 21 34 55
 //递归方法，容易溢出
 #include <stdio.h>
 #include <string.h>
@@ -95,4 +96,33 @@ int main() {
 	int rest = Fib(n);
 	printf("rest = %d\n", rest);
 	return 0;
+}
+
+
+//当然，我知道汉诺塔问题。汉诺塔是一个经典的数学问题或者说是游戏，来自于印度的传说。
+//在这个问题中，你有三个柱子和若干个大小不一的盘子。初始时，所有的盘子都在一个柱子上，且由下到上按大小排列。
+//问题的目标是将所有的盘子移动到另一个柱子上，每次只能移动一个盘子，且任何时刻大盘子都不能在小盘子上面。
+//这个问题的解法是递归的：首先解决 n-1 个盘子的问题，然后移动最大的盘子，再解决 n-1 个盘子的问题。
+
+//在这个代码中，hanoi函数用于移动盘子。
+//当只有一个盘子时，直接将盘子从源柱子移到目标柱子。
+//当有多于一个盘子时，先将n-1个盘子从源柱子移动到辅助柱子，
+//然后将最大的盘子从源柱子移动到目标柱子，
+//最后再将n-1个盘子从辅助柱子移动到目标柱子。
+#include <stdio.h>
+
+void hanoi(int n, char from_rod, char to_rod, char aux_rod) {
+    if (n == 1) {
+        printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod);
+        return;
+    }
+    hanoi(n - 1, from_rod, aux_rod, to_rod);
+    printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod);
+    hanoi(n - 1, aux_rod, to_rod, from_rod);
+}
+
+int main() {
+    int n = 4;  // Number of disks
+    hanoi(n, 'A', 'C', 'B');  // A, B and C are names of rods
+    return 0;
 }
