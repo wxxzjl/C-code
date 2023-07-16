0bf99af2dea6ae1148893ed1272bf2cfecd66822
张彧 <106905146+wxxzjl@users.noreply.github.com>
2023-07-16 17:43:45 +0800

    Create c语言学习6.c

---
+++ c语言学习6.c
@@ -0,0 +1,11 @@
+//例题
+//输出：-1 -1 255(整型提升，有无符号位)
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	char a = -1;
+	signed char b = -1;
+	unsigned char c = -1;
+	printf("%d %d %d ", a, b, c);
+	return 0;
+}
