3208247d12bcc8e730b3bfc8b47a8aa0f2ac8fcd
张彧 <106905146+wxxzjl@users.noreply.github.com>
2023-07-20 23:56:37 +0800

    Update c语言学习6.c

--- c语言学习6.c
+++ c语言学习6.c
@@ -175,3 +175,20 @@ int main() {
 	printf("%d\n", sizeof(&arr[0]+1));//（4/8）&arr[0]+1取出第二个元素的地址
 	return 0;
 }
+
+
+//例题
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h> 
+#include<string>
+int main() {
+	char arr[] = { 'a','b','c','d','e','f'};
+	printf("%d\n", strlen(arr));//随机值
+	printf("%d\n", strlen(arr+0));//随机值
+	printf("%d\n", strlen(*arr));//'a'地址->97非法访问
+	printf("%d\n", strlen(arr[1]));//'b'地址->98非法访问
+	printf("%d\n", strlen(&arr));//随机值
+	printf("%d\n", strlen(&arr+1));//随机值，从该数组末地址往后
+	printf("%d\n", strlen(&arr[0]+1));//随机值，从第二个元素往后
+	return 0;
+}
